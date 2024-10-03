68b705517d5debae0102426738595b6c2f5f2be0
miklisanton <miklis2003@mail.ru>
2024-10-03 19:20:05 +0300

    fix: dark mode disabled

--- components/SingleTracker.tsx
+++ components/SingleTracker.tsx
@@ -118,7 +118,7 @@ const Tracker = ({ tracker, isTelegramConnected} : {tracker: TrackerClass, isTel
             onChange={handleNotificationToggle}
             checked={notifications}
             className="sr-only peer"/>
-          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-700"></div>
+          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-700"></div>
           <span className="ms-3 text-sm font-medium text-gray-100 ">Telegram notifications</span>
         </label>
         <button type="button" onClick={handleRemove}>
--- components/TrackerForm.tsx
+++ components/TrackerForm.tsx
@@ -163,7 +163,7 @@ const TrackerForm = ({isTelegramConnected}:{isTelegramConnected: boolean}) => {
                 name="notifications"
                 checked={notifications}
                 className="sr-only peer"/>
-              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-700"></div>
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-700"></div>
             </label>
             <span className="text-sm font-medium text-gray-800 ml-2">Telegram <br/> notifications</span>
           </div>
