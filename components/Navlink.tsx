734b1c1fbbb64491082b29e1e0f34b597f381e7b
miklisanton <miklis2003@mail.ru>
2025-02-04 21:19:29 +0300

    fix: prefetch set to false for navlinks, to disable prefetching login/logout links

--- components/Navlink.tsx
+++ components/Navlink.tsx
@@ -7,7 +7,7 @@ export const NavLink = ({href, children }: {href:string, children:React.ReactNod
   const isActive = pathname === href
 
   return (
-    <Link href={href} legacyBehavior>
+    <Link href={href} prefetch={false} legacyBehavior>
       <a className={`flex justify-center text-md font-semibold leading-6 p-0 mr-2 ${isActive ? 'text-lime' : 'text-white'}`}>
         {children}
       </a>
